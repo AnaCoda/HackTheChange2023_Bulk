040a80f3e0d1640e9a8a88a700a4b0559af0b46d
AnaCoda <anacod@zoho.com>
2023-11-11 13:04:48 -0700

    Remove articles from model

--- src/flask-server/models.py
+++ src/flask-server/models.py
@@ -31,41 +31,20 @@ class User(db.Model):
     credits = db.Column(db.Integer, nullable=False)
     itemsForSale = db.relationship("ItemForSale", backref="user", lazy=True)
     
-
-class Articles(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100),nullable=False)
-    body = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime(), default=datetime.utcnow)
-
-
-    def __repr__(self):
-        return "<Articles %r>" % self.title
-# Generate marshmallow Schemas from your models
-class ArticlesShema(ma.Schema):
-    class Meta:
-        # Fields to expose
-        fields = ("id","title", "body", "date")
-class UsersShema(ma.Schema):
-    class Meta:
-        # Fields to expose
-        fields = ("id","firstName","lastName", "description", "email", "password", "credits", "posts")
 class ItemForSaleShema(ma.Schema):
     class Meta:
         # Fields to expose
         fields = ("id","name", "image", "price", "amount"
         ,"receipt", "description", "date_posted", "expiry_date","user_id")
+class UsersShema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ("id","firstName","lastName", "description", "email", "password", "credits", "itemsForSale")
+    itemsforSale = ma.Nested(ItemForSaleShema, many=True)
 
 class ReservationsShema(ma.Schema):
     class Meta:
         fields = ("id", "user_id", "itemForSale", "itemForSale_id", "reservedAmount")
-
-# IMPORTANT: These events are for instantiating DEFAULT values in a database. Especially helpful in a hackathon to sync SQLite
-@event.listens_for(Articles.__table__, 'after_create')
-def create_articles(*args, **kwargs):
-    db.session.add(Articles(id=1, title='abc@domain.com', body="hello"))
-    db.session.add(Articles(id=2, title='abcd@domain.com', body="helloo"))
-    db.session.commit()
 
 @event.listens_for(ItemForSale.__table__, 'after_create')
 def create_eyewear(*args, **kwargs):
