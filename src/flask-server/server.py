6a7da6baf62339e36a774d6212af2bd8a91bd7dd
AnaCoda <anacod@zoho.com>
2023-11-11 12:35:25 -0700

    Added upload route

--- src/flask-server/server.py
+++ src/flask-server/server.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import current_app,jsonify,request
 from app import create_app,db
 from models import ItemForSale, Reservations, User, users_schema,user_schema, itemForSale_schema, itemsForSale_schema, reservation_schema, reservations_schema
@@ -28,7 +29,7 @@ def user():
 	return jsonify(results)
 
 
-@app.route("/catalogPosts", methods=["GET", "POST"], strict_slashes=False)
+@app.route("/catalogPosts", methods=["GET"], strict_slashes=False)
 def eyewear():
 	items = ItemForSale.query.all()
 	for glass in items:
@@ -41,6 +42,17 @@ def eyewear():
 		print(results)
 		print(jsonify(results))
 		return jsonify(results)
+
+@app.route("/uploadCatalogPost", methods=["POST"])
+def uploadGlasses():
+	# USER ID IS CURRENTLY HARDCODED
+	db.session.add(ItemForSale(name = request.form.get('name'), image=request.form.get('image'), price=float(request.form.get('price'))),
+				amount=int(request.form.get('amount'), receipt=request.form.get('receipt'), description=request.form.get('description')),
+				expiry_date=datetime.datetime(request.form.get('expiry'), user_id=0))
+	resp = jsonify(success=True)
+	db.session.commit()
+	return resp
+
 	
 if __name__ == "__main__":
 	app.run(debug=True)
